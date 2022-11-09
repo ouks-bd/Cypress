311fff6cdb6fac7dd0ab4efe136c3f43245d975e
ouks-bd <obyed.oss@gmail.com>
2022-11-09 08:12:51 +0600

    url

--- Cypress/cypress/e2e/test.cy.js
+++ Cypress/cypress/e2e/test.cy.js
@@ -1,14 +1,22 @@
 /// <reference types="cypress" />
-
+describe("home", () => {
+    
 context('Assertions', () => {
     beforeEach(() => {
       cy.visit('https://www.daraz.com.bd/#')
     })
-    describe("home", () => {
+   
       it("home accessible", () => {
         cy.visit('https://www.daraz.com.bd/#');
        // cy.get('anonLogin').click
         cy.get('#anonLogin').click()
+        
+        cy.xpath("//input[@placeholder='Please enter your Phone Number or Email']").type('Naruto')
+
+       // cy.get("#a2a0e.login_signup.0.i0.4b002829EiLBdz").type("shoeb");
+        // cy.get("#a2a0e.login_signup.0.i5.4b002829EiLBdz").type("Password");
+        // cy.get('#a2a0e.login_signup.0.i6.4b002829EiLBdz').click()
+        
        // cy.get('button[id="anonLogin"]').click()
       //  cy.get('.anonLogin').click()
         //We fill the login FORM
