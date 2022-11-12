766f32be4e15503376446098b5d0d54d995b5f56
ouks-bd <obyed.oss@gmail.com>
2022-11-12 15:29:32 +0600

    log-in

--- Cypress/cypress/e2e/test.cy.js
+++ Cypress/cypress/e2e/test.cy.js
@@ -8,25 +8,12 @@ context('Assertions', () => {
    
       it("home accessible", () => {
         cy.visit('https://www.daraz.com.bd/#');
-       // cy.get('anonLogin').click
+       
         cy.get('#anonLogin').click()
-
-        cy.get("#text").type("Automation Bro");
-       // cy.xpath("//input[@placeholder='Please enter your Phone Number or Email']").type("test")
-    
-      // cy.get("#a2a0e.login_signup.0.i0.4b002829EiLBdz").type("shoeb");
-        // cy.get("#a2a0e.login_signup.0.i5.4b002829EiLBdz").type("Password");
-        // cy.get('#a2a0e.login_signup.0.i6.4b002829EiLBdz').click()
-        
-       // cy.get('button[id="anonLogin"]').click()
-      //  cy.get('.anonLogin').click()
-        //We fill the login FORM
-        // cy.get("#username").type("");
-        // cy.get("#password").type("");
-           
-        // cy.get(".btn.btn-primary.btn-lg").click();
-        // cy.visit('');
-        // cy.visit('');
+        cy.get('.mod-login-input-loginName >input ').type("Automation Bro");
+        cy.get('.mod-input-password > input').type("test");
+        cy.get('.next-btn').click
+       
       });
     });
   
